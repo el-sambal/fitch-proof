fe8295faae957b76d652ef78d4982cd7b220a3ab
el-sambal <AronHardeman@proton.me>
2024-03-15 07:22:47 +0100

    rename file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod parser;
+mod logic_expr_parser;
 fn main() {
     println!(
         "{:?}",
-        parser::parse_logical_expression_string(
+        logic_expr_parser::parse_logical_expression_string(
             //"∀ x∀ x(P(a,b,x)→Q(f(a),f(b,c,d),g(x)))∨f(a,b)=f(bla,c)"
             "∀ x(P(a,b,x)→Q(f(a),f(b,c,d),g(x)))∨f(a,b)=f(bla,c)∨¬∃x¬¬¬∃y¬¬∀z¬¬(P(f(x),f(y),f(z))→¬(A(x)∧B(y)))"
         )
