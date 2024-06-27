f1eb97ee410ef00b56bbfb35aa49c223d5884438
el-sambal <AronHardeman@proton.me>
2024-06-27 16:56:44 +0200

    take template over stdin

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -14,10 +14,10 @@ This is meant for whoever manages Themis:
 
 Oops, something went wrong. This is how the program is supposed to be called:
 
-      ./fitch-checker <proof.txt> <template.txt>
+      ./fitch-checker proof.txt < template.txt
 OR
-      ./fitch-checker <proof.txt> <template.txt> <variables.txt>
-
+      ./fitch-checker proof.txt variables.txt < template.txt
+ 
 Of course, <proof.txt> could be any filename, but we will call 
 it proof.txt in the text below. The same applies for the other files.
 
@@ -26,7 +26,7 @@ would write it in the web interface. The file template.txt should
 contain only a number of logical sentences, namely (in order) the
 premises and the final conclusion that the proof should lead to.
 The idea is that the proof in proof.txt must match the template
-in template.txt.
+in template.txt. The template should be given via stdin.
 
 It is optional to provide the file variables.txt. If provided, this file
 should contain the strings that should be seen as a variable. The file
@@ -53,22 +53,18 @@ fn fail_open_file(filename: &str) -> ! {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 && args.len() != 4 {
+    if args.len() != 2 && args.len() != 3 {
         usage()
     }
     let proof_filename = &args[1];
-    let template_filename = &args[2];
     let Ok(proof) = std::fs::read_to_string(proof_filename) else {
         fail_open_file(proof_filename)
     };
-    let Ok(template) = std::fs::read_to_string(template_filename) else {
-        fail_open_file(template_filename)
-    };
-    let template: Vec<String> = template.lines().map(|s| s.trim().to_string()).collect();
-    let variables = if args.len() == 3 {
+    let template: Vec<String> = std::io::stdin().lines().map(|s| s.unwrap().trim().to_string()).collect();
+    let variables = if args.len() == 2 {
         DEFAULT_ALLOWED_VARIABLE_NAMES.to_string()
     } else {
-        let variables_filename = &args[3];
+        let variables_filename = &args[2];
         let Ok(variables) = std::fs::read_to_string(variables_filename) else {
             fail_open_file(variables_filename)
         };
